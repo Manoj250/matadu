d00eb1ec24c4896e3d47646095e7c6a740e68950
Manoj250 <manojdbhat250@gmail.com>
2025-04-23 00:22:17 +0530

    created a sample socket. This is a fake programming langugae.

--- socket_gateway/lib/socket_gateway_web/endpoint.ex
+++ socket_gateway/lib/socket_gateway_web/endpoint.ex
@@ -12,8 +12,12 @@ defmodule SocketGatewayWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]], 
+    websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
+
+  socket "/chat", SocketGatewayWeb.UserSocket,
+    websocket: true,
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
